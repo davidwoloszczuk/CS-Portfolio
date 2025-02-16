a67f767267490ea20e5799c4a6717c2e6341d80b
David Woloszczuk-Mrugala <davidwoloszczuk@gmail.com>
2025-02-15 18:16:40 -0600

    npm start works

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css'; // Import any CSS for the sidebar
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* Your sidebar content here */}
+    </div>
+  );
+};
+
+export default Sidebar;
