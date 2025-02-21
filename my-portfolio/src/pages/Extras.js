6351e0f2d8181a63f05d1f42e80824b78c90c16a
David Woloszczuk-Mrugala <davidwoloszczuk@gmail.com>
2025-02-20 20:29:00 -0600

    Adding Pages

---
+++ my-portfolio/src/pages/Extras.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Extras() {
+  return (
+    <div id="extras">
+      <h1>Extras</h1>
+      <p>This is the Extras page.</p>
+    </div>
+  );
+}
+
+export default Extras;
---
+++ my-portfolio/src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Settings() {
+  return (
+    <div id="settings">
+      <h1>Settings</h1>
+      <p>This is the Settings page.</p>
+    </div>
+  );
+}
+
+export default Settings;
